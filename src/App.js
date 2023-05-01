5292b779d9366dcc3394015afcc46cc7a6dd7185
Naveen Kumar <vvnnkk2600@gmail.com>
2023-05-01 17:54:28 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 
 
 function App() {
-  const links = [
-    { href: '', label: 'Contact' },
-    { href: '', label: 'AboutMe' },
-    { href: 'https://github.com/Naveen17k?tab=repositories', label: 'Products' },
-  ]
+ 
   return (
 
     <>
@@ -16,22 +12,22 @@ function App() {
         <h1 className='text-white text-4xl text-center mt-10'>Hi I'm Naveen Kumar</h1>
         <h1 className='text-white text-4xl text-center mt-10'>Check Out My Links</h1>
         <center>
-        <div className='grid grid-rows-4 text-white mt-10 w-fit  '>
-          
-            <button onClick={()=>{window.location.href="https://github.com/Naveen17k"}} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+          <div className='grid grid-rows-4 text-white mt-10 w-fit  '>
+
+            <button onClick={() => { window.location.href = "https://github.com/Naveen17k" }} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
               GitHub
             </button>
-            <button onClick={()=>{window.location.href="https://t.me/Naveen1110k"}} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={() => { window.location.href = "https://t.me/Naveen1110k" }} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
               Telegram
             </button>
-            <button onClick={()=>{window.location.href="https://t.me/+WKbYTwqB40oyNDNl"}} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded\">
-             Developement Discussion
+            <button onClick={() => { window.location.href = "https://t.me/+WKbYTwqB40oyNDNl" }} className="mb-2 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded\">
+              Developement Discussion
             </button>
-            <button onClick={()=>{window.location.href="https://www.instagram.com/n_aveen.nk/"}} className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+            <button onClick={() => { window.location.href = "https://www.instagram.com/n_aveen.nk/" }} className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
               Instagram
             </button>
-          
-        </div>
+
+          </div>
         </center>
       </div>
     </>
